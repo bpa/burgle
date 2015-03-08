51a7afa55819c4e353518b7770ba81c05dcf4049
Bruce Armstrong <bruce@armstronganchor.net>
2015-03-07 22:41:09 -0700

    Add optional heatmap optional, create permalink Fixes #1,#2,#3

--- burgle.js
+++ burgle.js
@@ -1,5 +1,6 @@
 "use strict";
 var Burgle = (function () {
+    var show_heat = false;
 
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -8,7 +9,7 @@ var Burgle = (function () {
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
 
-    function toFloor(walls) {
+    function to_floor(walls) {
         var i = 0;
         var floor = Array(17).join(1).split('').map(function () {
             return {heat: 0}
@@ -90,49 +91,67 @@ var Burgle = (function () {
         return dist;
     }
 
-    function find_clockwise(options, to) {
+    function find_clockwise(from, to, options) {
+        var dy = Math.floor(to / 4) - Math.floor(from / 4);
+        var dx = to % 4 - from % 4;
+        var target = Math.atan2(dy, dx);
+        var min = 7;
+        var dir;
         for (var i = 0; i < options.length; i++) {
-            var dy = 1, dx = 1;
-            var r = Math.atan2(dy, dx);
+            var o = options[i];
+            var r = o[0] - target;
+            if (r < 0)
+                r += 2 * Math.PI;
+            if (r < min) {
+                min = r;
+                dir = o[1];
+            }
         }
-        return options[0];
+        return dir;
     }
 
     function walk(from, to, floor, dist) {
         var min, shortest, tile;
-        function look(dir, neighbor) {
+        function look(dir, neighbor, r) {
             var ind = neighbor * 16 + to;
             if (tile[dir]) {
                 if (dist[ind] < min) {
-                    shortest = [neighbor];
+                    shortest = [[r, neighbor]];
                     min = dist[ind];
                 }
                 else if (dist[ind] === min) {
-                    shortest.push(neighbor);
+                    shortest.push([r, neighbor]);
                 }
             }
         }
         while (from !== to) {
             min = 20;
             tile = floor[from];
-            look('n', from - 4);
-            look('e', from + 1);
-            look('s', from + 4);
-            look('w', from - 1);
-            var next = shortest.length > 1 ? find_clockwise(shortest) : shortest[0];
+            look('n', from - 4, Math.PI * -.5);
+            look('e', from + 1, 0);
+            look('s', from + 4, Math.PI * .5);
+            look('w', from - 1, Math.PI);
+            var next = shortest.length > 1 ? find_clockwise(from, to, shortest) : shortest[0][1];
             floor[next].heat++;
             from = next;
         }
     }
 
     function heatmap(id, floor) {
+        if (!show_heat) {
+            for (var i = 0; i < 16; i++) {
+                document.getElementById(id + '_t' + i).style.backgroundColor = '';
+            }
+            return;
+        }
+
         var dist = build_distance(floor);
         for (var i = 0; i < 16; i++) {
             for (var j = 0; j < 16; j++) {
                 walk(i, j, floor, dist);
             }
         }
-        for (var i = 0; i< 16; i++) {
+        for (var i = 0; i < 16; i++) {
             var heat = (1.0 - (floor[i].heat - 15) / 168) * 240;
             document.getElementById(id + '_t' + i).style.backgroundColor = 'hsl(' + heat + ',100%,50%)';
         }
@@ -145,30 +164,44 @@ var Burgle = (function () {
             heat.push(r);
         }
         console.table(heat);
-		var total_heat = 0;
-		for (var i=0; i<16; i++) {
-			total_heat += floor[i].heat;
-		}
-		console.log(total_heat);
+        var total_heat = 0;
+        for (var i = 0; i < 16; i++) {
+            total_heat += floor[i].heat;
+        }
+        console.log(total_heat);
     }
 
-    function set_layout(id, layout) {
+    function as_walls(layout) {
         var walls = [];
-        for (var i=0; i<24; i++) {
+        for (var i = 0; i < 24; i++) {
             if ((1 << i) & layout) {
                 walls[i] = true;
             }
         }
-        var floor = toFloor(walls);
+        return walls;
+    }
+
+    function set_layout(id, layout, walls) {
+        if (walls === undefined)
+            walls = as_walls(layout);
+        var floor = to_floor(walls);
         if (!valid(floor))
-            return;
+            return false;
+        var f = document.getElementById(id);
+        f.setAttribute('layout', layout.toString(16));
         for (var w = 0; w < 24; w++) {
             document.getElementById(id + '_' + w).className = walls[w] ? 'wall' : '';
         }
         heatmap(id, floor);
+        return true;
     }
 
-    function initFloors() {
+    function init_floors() {
+        if (getParameterByName('heat') !== "") {
+            show_heat = true;
+            var heat = document.getElementById('burgle_heat');
+            if (heat !== null) heat.checked = show_heat;
+        }
         var floors = document.getElementsByClassName("floor");
         for (var f = 0; f < floors.length; f++) {
             var floor = floors[f];
@@ -197,44 +230,74 @@ var Burgle = (function () {
                 table.appendChild(row);
             }
             floor.appendChild(table);
-            var layout = getParameterByName(id);
-            if (layout !== '')
-                set_layout(id, layout);
+            var layout = parseInt(getParameterByName(id), 16);
+            if (layout > 0 && set_layout(id, layout))
+                floor.setAttribute('layout', layout.toString(16));
         }
+        update_href();
+    }
+
+    function update_href() {
+        var link = window.location.protocol + '//';
+        link += window.location.hostname;
+        if (window.location.port)
+            link += ':' + window.location.port;
+        link += window.location.pathname + '?';
+        if (show_heat)
+            link += 'heat=on&';
+
+        var layouts = [];
+        var floors = document.getElementsByClassName("floor");
+        for (var f = 0; f < floors.length; f++) {
+            layouts.push(floors[f].getAttribute('id') + '=' + floors[f].getAttribute('layout'));
+        }
+        console.log(link);
+
+        document.getElementById('burgle_href').href = link + layouts.join('&');
     }
 
     if (window.addEventListener)
-        window.addEventListener('load', initFloors, false);
+        window.addEventListener('load', init_floors, false);
     else
-        window.attachEvent('onload', initFloors); //IE
+        window.attachEvent('onload', init_floors); //IE
 
-    return function (id) {
-        var floors;
-        if (id === undefined || id === 'all')
-            floors = document.getElementsByClassName("floor");
-        else
-            floors = [document.getElementById(id)];
-        for (var f = 0; f < floors.length; f++) {
-            while (true) {
-                var walls = [];
-                for (var w = 0; w < 8; ) {
-                    var n = Math.floor(Math.random() * 24);
-                    if (!walls[n]) {
-                        w++;
-                        walls[n] = true;
+    return {
+        show_heat: function (show) {
+            show_heat = show;
+            var floors = document.getElementsByClassName("floor");
+            for (var f = 0; f < floors.length; f++) {
+                var layout = floors[f].getAttribute('layout');
+                if (layout !== null) {
+                    set_layout(floors[f].getAttribute('id'), parseInt(layout, 16));
+                }
+            }
+            update_href();
+        },
+        generate: function (id) {
+            var floors;
+            if (id === undefined || id === 'all')
+                floors = document.getElementsByClassName("floor");
+            else
+                floors = [document.getElementById(id)];
+            for (var f = 0; f < floors.length; f++) {
+                while (true) {
+                    var walls = [];
+                    var layout = 0;
+                    for (var w = 0; w < 8; ) {
+                        var n = Math.floor(Math.random() * 24);
+                        if (!walls[n]) {
+                            w++;
+                            walls[n] = true;
+                            layout |= 1 << n;
+                        }
+                    }
+                    if (set_layout(floors[f].getAttribute('id'), layout, walls)) {
+                        break;
                     }
                 }
-                var floor = toFloor(walls);
-                if (!valid(floor))
-                    continue;
-                var id = floors[f].getAttribute('id');
-                for (var w = 0; w < 24; w++) {
-                    document.getElementById(id + '_' + w).className = walls[w] ? 'wall' : '';
-                }
-                heatmap(id, floor);
-                break;
             }
+            update_href();
         }
-    }
+    };
 
 }());
